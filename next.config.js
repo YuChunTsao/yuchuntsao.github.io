ab4f4eed2d59cd85cce8813aed119f1ed9afd8b6
Yu Chun Tsao <tsao84672776@gmail.com>
2023-02-14 00:23:52 +0800

    fix bug - 404 page without next.js default

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app;
+  script-src * blob: 'self' 'unsafe-eval' 'unsafe-inline' giscus.app;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src 'none';
@@ -60,6 +60,7 @@ module.exports = withBundleAnalyzer({
   exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
     return {
       '/': { page: '/' },
+      '/404': { page: '/404' },
     }
   },
   reactStrictMode: true,
